d1b05ee0afff1a4b7f2d4c16714d49044189811c
Leonardi dallOcca dellOrso <fa23811ed5dc@rpi-buster-armhf>
2023-10-19 15:57:53 +0000

    Module8 fourth commit

--- Module8/libConversions.s
+++ Module8/libConversions.s
@@ -1,4 +1,26 @@
 .global Ft2Inches
+.global F2C
+
+.text
+F2C:
+   # r0 = (r0-32)*5/9
+   MOV r1, #-32
+   ADD r0, r0, r1
+   MOV r1, #5
+   MUL r0, r0, r1
+   MOV r1, #9
+   BL __aeabi_idiv
+   // result will be in r0
+
+   # garbage code to illustrate something in lecture
+   ADD r3, r3, r3
+
+   MOV pc, lr
+
+.data
+
+#End F2C
+
 .text
 
 Ft2Inches:

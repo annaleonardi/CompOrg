da34cdc32dd823b15e865055f1360f2b79a16fde
Leonardi dallOcca dellOrso <fa23811ed5dc@rpi-buster-armhf>
2023-10-29 19:30:08 +0000

    Module9 18th commit

--- Module9/charCheck2.s
+++ Module9/charCheck2.s
@@ -4,12 +4,37 @@ main:
    SUB sp, sp, #4
    STR lr, [sp], #0
 
-   MOV r0, #'U'
+#  prompt for value
+   LDR r0, =prompt
+   BL printf
+
+#  read value  from user
+   LDR r0, =format
+   LDR r1, =char
+   BL scanf
+
+###   MOV r0, #'U'
+   LDR r0, =char
+   LDR r0, [r0]
    BL charCheck
 
    LDR lr, [sp, #0]
    ADD sp, sp, #4
    MOV pc, lr
+
+.data
+   prompt: .asciz "Enter any character: \n"
+   char: .space 40
+   format: .asciz "%s"
+
+#  END main
+
+
+
+.text
+#  Push stack
+   SUB sp, sp, #4
+   STR lr, [sp]
 
    charCheck:
 	SUB sp, sp, #4

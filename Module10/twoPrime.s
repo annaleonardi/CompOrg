bd7c9f3e375fe2a24135e47d642d3ed2d53af9c8
Leonardi dallOcca dellOrso <fa23811ed5dc@rpi-buster-armhf>
2023-11-06 00:09:19 +0000

    Module10 commit 4

---
+++ Module10/twoPrime.s
@@ -0,0 +1,74 @@
+.text
+.global main
+
+main:
+    SUB sp, sp, #12
+    STR lr, [sp, #4]
+
+inputLoop:
+    LDR r0, =prompt
+    BL printf
+    LDR r0, =input
+    LDR r1, =num
+    BL scanf
+    LDR r1, =num
+    LDR r4, [r1, #0]
+
+    CMP r4, #-1
+    BEQ endProgram
+
+    CMP r4, #2
+    BLE invalidInput
+
+    MOV r5, #2  // r5 will be used as a counter for divisors
+
+primeLoop:
+    CMP r5, r4
+    BEQ isPrime
+
+    MOV r0, r4
+    MOV r1, r5
+    BL checkDivisibility
+
+    CMP r0, #0
+    BEQ notPrime
+
+    ADD r5, r5, #1
+    B primeLoop
+
+checkDivisibility:
+    BL __aeabi_idivmod
+    MOV r0, r1
+    BX lr
+
+isPrime:
+    LDR r0, =primeMsg
+    MOV r1, r4
+    BL printf
+    B inputLoop
+
+notPrime:
+    LDR r0, =notPrimeMsg
+    MOV r1, r4
+    BL printf
+    B inputLoop
+
+invalidInput:
+    LDR r0, =badInput
+    BL printf
+    B inputLoop
+
+endProgram:
+    MOV r0, #0
+    LDR lr, [sp, #4]
+    ADD sp, sp, #12
+    MOV pc, lr
+
+.data
+prompt: .asciz "Please enter a number (-1 to exit): "
+badInput: .asciz "Invalid input. Enter a number greater than 2 or -1.\n"
+primeMsg: .asciz "Number %d is prime.\n"
+notPrimeMsg: .asciz "Number %d is not prime.\n"
+input: .asciz "%d"
+num: .word 0
+
